10f155db2eca9a08a803f24c586bdaf943563d0e
Vitor Lopes <vitor.lopes@bhs.com.br>
2020-05-29 17:46:11 -0300

    Iniciando estilização.

---
+++ Frontend/app-tobehero/src/pages/Register/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default function Register() {
+  return (
+      <div className="register-container">
+          <div className="content">
+            <section>
+                <img src={logoImg} alt="Be the Hero"/>
+                <h1>Cadastro</h1>
+                <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </Link>
+            </section>
+            <form>
+                <input type="text" placeholder="Nome da ONG" />
+                <input type="text" placeholder="E-mail" />
+                <input type="text" placeholder="Whatsapp" />
+
+                <div className="input-group">
+                    <input type="text" placeholder="Cidade"/>
+                    <input type="text" placeholder="UF" style={{ width: 80 }}/>
+                </div>
+
+                <button className="button" type="submit">Cadastrar</button>
+            </form>
+          </div>
+      </div>
+  );
+}
