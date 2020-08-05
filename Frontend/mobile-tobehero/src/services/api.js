19b6218114b039149c0d623f51404677d89b18fa
Vitor Lopes <vitoraugustoa@hotmail.com>
2020-08-05 15:01:08 -0300

    Comentando configuração do serviço axios para mobile.

--- Frontend/mobile-tobehero/src/services/api.js
+++ Frontend/mobile-tobehero/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Adicionar o IP da maquina para realizar a requisição
+// em desenvolvimento.
+// Caso esteja utilizando .NET/.NET Core para API
+// Alterar o APP URL para o IP da maquina e a porta 5000 ou 5001  
 const api = axios.create({
-  baseURL: `http://192.168.100.5:5000`,
+  baseURL: `http://localhost:5000`,
   headers: {"Authorization": "1623335306"}
 });
 
